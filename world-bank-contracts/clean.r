edae8c3bbe5ed37c998506a96507298795db24c9
Thomas Levine <occurrence@thomaslevine.com>
2013-09-29 03:20:28 -0700

    aoeu

--- world-bank-contracts/clean.r
+++ world-bank-contracts/clean.r
@@ -1,9 +1,13 @@
 library(reshape2)
+library(plyr)
 
 if (!('a' %in% ls())) {
   a <- read.csv('major-contract-awards.csv', stringsAsFactors = FALSE)
   a$As.of.Date <- strptime(a$As.of.Date, '%m/%d/%Y 12:00:00 AM')
   a$Contract.Signing.Date <- strptime(a$Contract.Signing.Date, '%m/%d/%Y 12:00:00 AM')
+
+  a$Country <- a$Borrower.Country
+  a$Year <- a$Fiscal.Year
 }
 
 if (!('gdp' %in% ls())) {
@@ -18,3 +22,8 @@ if (!('gdp' %in% ls())) {
   gdp$GDP <- as.numeric(gdp$GDP) * 1e9
   gdp$Year <- as.numeric(sub('^X', '', as.character(gdp$Year)))
 }
+a.agg <- ddply(a, c('Year', 'Country'), function(df) {
+  c(Count = nrow(df))
+})
+
+x <- join(na.omit(gdp), a.agg, by = c('Year', 'Country'))
