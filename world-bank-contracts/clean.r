b24a548faf00a2ee739567b34aebf0b048b3051f
Thomas Levine <occurrence@thomaslevine.com>
2013-09-29 03:12:20 -0700

    clen up gdp

--- world-bank-contracts/clean.r
+++ world-bank-contracts/clean.r
@@ -1,3 +1,5 @@
+library(reshape2)
+
 if (!('a' %in% ls())) {
   a <- read.csv('major-contract-awards.csv', stringsAsFactors = FALSE)
   a$As.of.Date <- strptime(a$As.of.Date, '%m/%d/%Y 12:00:00 AM')
@@ -5,5 +7,12 @@ if (!('a' %in% ls())) {
 }
 
 if (!('gdp' %in% ls())) {
-  gdp <- read.csv('gdp.tsv', sep = '\t')
+  .gdp.wide <- read.csv('gdp.tsv', sep = '\t')
+  .columns <- c('Country', 'Subject.Descriptor', 'Units', 'Scale', 'Country.Series.specific.Notes', 'Estimates.Start.After')
+  gdp <- melt(gdp.wide, .columns, variable.name = 'Year', value.name = 'GDP')
+  gdp$Scale <- NULL # billions
+  gdp$Subject.Descriptor <- NULL # GDP
+  gdp$Estimates.Start.After <- NULL
+  gdp$Country.Series.specific.Notes <- NULL
+  gdp$Year <- as.numeric(sub('^X', '', as.character(gdp$Year)))
 }
