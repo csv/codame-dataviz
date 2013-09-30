81a4d9159cfdcd8889212ce9623bcf6b4d35fe01
Thomas Levine <occurrence@thomaslevine.com>
2013-09-30 11:40:09 -0700

    structuring the song

---
+++ world-bank-contracts/structure.hs
@@ -0,0 +1,42 @@
+type Beat    = Integer -- Placeholder
+type Measure = (Beat, Beat, Beat, Beat)
+type Stanza  = (Measure, Measure, Measure, Measure,
+                Measure, Measure, Measure, Measure)
+type Song    = [Stanza]
+
+stanza :: Maybe Integer -> ()
+stanza = ()
+
+main <- do
+  song   = [ stanza Nothing,
+             stanza 2000,
+             stanza 2000,
+             stanza 2001,
+             stanza 2001,
+             stanza 2002,
+             stanza 2002,
+             stanza 2003,
+             stanza 2003,
+             stanza 2004,
+             stanza 2004,
+             stanza 2005,
+             stanza 2005,
+             stanza 2006,
+             stanza 2006,
+             stanza 2007,
+             stanza 2007,
+             stanza 2008,
+             stanza 2008,
+             stanza 2009,
+             stanza 2009,
+             stanza 2010,
+             stanza 2010,
+             stanza 2011,
+             stanza 2011,
+             stanza 2012,
+             stanza 2012,
+             stanza 2013,
+             stanza 2013 ]
+  nbeats = (length song) * 8 * 4
+  bpm    = 256
+  minutes= bpm / nbeats
