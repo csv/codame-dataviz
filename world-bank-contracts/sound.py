37fd821091cfc8d97c6bfb0be22960e9418a8d8c
Thomas Levine <occurrence@thomaslevine.com>
2013-10-03 15:28:04 +0000

    writ emidi

--- world-bank-contracts/sound.py
+++ world-bank-contracts/sound.py
@@ -1,29 +1,6 @@
 import math
 from midiutil.MidiFile import MIDIFile
 
-bpm = 120
-midi_track = MIDIFile(1)
-midi_track.addTempo(track=0, time=0,tempo=bpm)
-
-beat = bpm_time(bpm, count='1/4')
-t = 0
-
-for d in date_range:
-    day = d.strftime('%Y-%m-%d')
-    if crashes.has_key(day):
-        for crash in crashes[day]:
-            key = hex_to_key[crash['hex_id']]
-            print key
-            midi_track.addNote(
-                track=0,
-                channel=0,
-                pitch=key,
-                duration=beat,
-                volume=100,
-                time=t
-            )
-    t += beat
-
 def fraction_to_midi(fraction):
     '''
     Args:
@@ -57,3 +34,14 @@ def add_phrase(beat, midi_track, time, phrase):
                 time=time
             )
     return midi_track, (time + beat)
+
+bpm = 120
+midi_track = MIDIFile(1)
+midi_track.addTempo(track=0, time=0,tempo=bpm)
+
+beat = bpm_time(bpm, count='1/4')
+t = 0
+
+binfile = open('bank.mid', 'wb')
+midi_track.writeFile(binfile)
+binfile.close()
